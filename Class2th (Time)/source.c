59b802278664787d79eae599d749bbdabeb74965
hxun202 <kangsojin0918@gmail.com>
2026-01-14 21:44:58 +0900

    2026.1.14 출력

---
+++ Class2th (Time)/source.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+
+int health = 5;
+
+void render()
+{
+	for (int i = 0; i < health; i++)
+	{
+		printf("♥");
+	}
+	if (health - 1)
+	{
+		printf("♡");
+	}
+}
+
+void shuffle(int array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int seed = rand() % size;
+
+		int temporary = array[seed];
+
+		array[seed] = array[i];
+
+		array[i] = temporary;
+	}
+}
+
+int main()
+{
+#pragma region 의사 난수
+	// 0~32767 사이의 난수 값을 반환하는 함수입니다.
+
+	// UTC 기준으로 1970년 1월 1일 0시 0분 0초부터 경과된 
+	// 시간을 초(sec)로 반환하는 함수입니다.
+
+	// srand : rand()가 사용할 초기값(seed)을 설정하는 함수
+
+	//srand(time(NULL));
+
+	//int random = rand() % 10 + 1;
+
+	//printf("random : %d\n", random);
+
+
+#pragma endregion
+
+#pragma region 셔플 함수
+
+	//srand(time(NULL));
+
+	//int list[] = { 0,1,2,3,4,5,6,7,8,9 };
+
+	//int size = sizeof(list) / sizeof(list[0]);
+
+	//shuffle(list, size);
+
+	//for (int i = 0; i < size; i++)
+	//{
+	//	printf("list[%d]의 값 : %d\n", i, list[i]);
+	//}
+
+#pragma endregion
+
+#pragma region UP - DOWN 게임
+
+	//int x = 1;
+	// int y = 50;
+
+	//	srand(time(NULL));
+
+	//	int random = rand() % 50 + 1;
+
+	//	int input = 0;
+
+	//		while (health>0)
+	//	{	
+	//			render();
+	//			printf("\n");
+	//			printf("현재 목숨 : %d\n", health);
+
+	//			printf("%d ~ %d 사이의 숫자를 입력하세요 : ", x, y);
+
+	//		scanf_s("%d", &input);
+
+	//		if (input < random)
+	//		{
+	//			printf("컴퓨터가 가지고 있는 값보다 작습니다.\n");
+	//			health--;
+	//			printf("\n\n");
+	//		}
+	//		else if (input > random)
+	//		{
+	//			printf("컴퓨터가 가지고 있는 값보다 큽니다.\n");
+	//			health--;
+	//			printf("\n\n");
+	//		}
+	//		else if (input == random)
+	//		{
+	//			printf("컴퓨터가 가지고 있는 값을 맞추었습니다.\n");
+	//			printf("Victory\n");
+
+	//			break;
+	//		}
+	//	}
+
+	//					 if (health <= 0)
+	//					{
+	//						printf("컴퓨터가 가지고 있는 값을 맞추지 못하였습니다\n");
+	//						printf("Defeat\n");
+	//						}
+
+
+#pragma endregion
+
+
+	return 0;
+}
