608f1b91e4da8a8d00ac304ad873e877f80d43e1
hxun202 <kangsojin0918@gmail.com>
2026-01-15 21:43:59 +0900

    2026.1.15 이중 버퍼링

--- Program/source.c
+++ Program/source.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+#define SIZE 1000
+
 int main()
 {
 #pragma region  파일 입출력
@@ -9,12 +11,41 @@ int main()
 	// 첫 번째 매개 변수 (파일의 이름)
 	// 두 번째 매개 변수 (파일의 입출력 모드)
 
-	FILE* file = fopen("data.txt", "w");
+	//FILE* file = fopen("data.txt", "w");
 
-	fputs("Character Information\n", file);
-	fputs("Health : \n", file);
-	fputs("Attack : \n", file);
-	fputs("Defense : \n", file);
+	//fputs("Character Information\n", file);
+	//fputs("Health : \n", file);
+	//fputs("Attack : \n", file);
+	//fputs("Defense : \n", file);
+
+	//fclose(file);
+
+#pragma endregion
+
+#pragma region 파일 읽기
+
+	FILE* file = fopen("data.txt", "r");
+
+	int count = 0;
+	char charcter = NULL;
+
+	while ((charcter = fgetc(file)) != EOF)
+	{
+		count++;
+	}
+
+	printf("count : %d\n", count);
+
+	char buffer[] = { 0 ,};
+
+	// 첫 번째 매개 변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번째 매개 변수 : 각 데이터 항목의 크기
+	// 세 번째 매개 변수 : 테이터를 읽어올 항목의 개수
+	// 네 번째 매개 변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	fread(buffer, sizeof(char), sizeof(buffer),file);
+
+	printf("%s", buffer);
 
 	fclose(file);
 
